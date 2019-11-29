3544284941d5f6aa3027c22ad98b869af1936284
Nils Bokermann <nils.bokermann@bermuda.de>
2019-11-29 19:26:56 +0100

    Add OTA.

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "../lib/EEPROM/EEPromStorage.h"
 #include "../lib/HTMLHandler/HTMLHandler.h"
 #include <HTMLHandler.h>
+#include <ArduinoOTA.h>
 
 
 #define BLINK_LED D5
@@ -64,6 +65,28 @@ void setup() {
     WiFi.hostname(ESPHostname); // Set the DHCP hostname assigned to ESP station.
     Serial.setDebugOutput(true); //Debug Output for WLAN on Serial Interface.
     startWifi();
+    ArduinoOTA.onStart([]() {
+        Serial.println("Start");
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+    ArduinoOTA.setPassword("Password");
+    ArduinoOTA.begin();
+    Serial.println("Ready");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void startWifi() {
@@ -157,8 +180,8 @@ byte ConnectWifiAP() {
     }
     if (connRes == 3) {
         Serial.print("STA ");
-        WiFi.setAutoReconnect(
-                true); // Set whether module will attempt to reconnect to an access point in case it is disconnected.
+        // Set whether module will attempt to reconnect to an access point in case it is disconnected.
+        WiFi.setAutoReconnect(true);
         // Setup MDNS responder
         if (!MDNS.begin(ESPHostname)) {
             Serial.println("Err: MDNS");
@@ -440,5 +463,8 @@ void loop() {
     }
     //HTTP
     server.handleClient();
+    ArduinoOTA.handle();
     yield();
 }
+
+
