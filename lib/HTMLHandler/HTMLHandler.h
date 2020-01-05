2889cada6f394948853a8482b60ba70b5bf5901d
Nils Bokermann <nils.bokermann@bermuda.de>
2020-01-05 19:33:38 +0100

    Workaround for ArduinoLog

--- lib/HTMLHandler/HTMLHandler.h
+++ lib/HTMLHandler/HTMLHandler.h
@@ -6,28 +6,49 @@
 #define ARDUINOWLANSWITCH_HTMLHANDLER_H
 
 #include <string>
+// Workaround for ArduinoLog in native test.
+#ifndef ARDUINO
+#define ARDUINO 1001
+#define UNSET_ARDUINO
+#endif // ARDUINO
 #include <ArduinoLog.h>
+#ifdef UNSET_ARDUINO
+#undef ARDUINO
+#endif // UNSET_ARDUINO
 #include "AbstractPersistence.h"
 
 #define MEDIATYPE_TEXT_HTML "text/html"
 #define MEDIATYPE_TEXT_PLAIN "text/plain"
 
 using namespace std;
+
 class HTMLHandler {
 public:
-    HTMLHandler(Persistence * persit, Logging *log);
+    HTMLHandler(Persistence *persit, Logging *log);
+
     ~HTMLHandler();
+
     string getMainPage();
+
     string getWifiSaveDonePage();
+
     string getCss();
+
     string getWifiPage();
+
     void resetWifiPage();
+
     string getSwitch(bool on);
-    void addAvailableNetwork(const string  &ssid, unsigned char encryption, int strength);
+
+    void addAvailableNetwork(const string &ssid, unsigned char encryption, int strength);
+
     void addRegisteredNetwork(const string &ssid);
+
     void setSoftAPCredentials(const string &ssid, const string &password);
+
 protected:
-    static void replaceString(string &original, const string &toReplace, const string &replacement) ;
+    static void replaceString(string &original, const string &toReplace, const string &replacement);
+
 private:
     const string internalError = "Internal Error. Cannot deliver page.";
     string options;
@@ -39,7 +60,8 @@ private:
     string registeredNetwork;
 
     string getStaticPage(const char *path) const;
-    Persistence * persistence;
+
+    Persistence *persistence;
     Logging *_log;
 };
 
