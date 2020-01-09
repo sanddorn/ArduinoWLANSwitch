b7f5cd9fa10dde84c9d6a79a449b4d4bac885d1b
bokerman <n.bokermann@verband.creditreform.de>
2020-01-09 12:04:02 +0100

    Tests on WifiConfig started

--- test/WifiConfig/test_WifiConfig.cpp
+++ test/WifiConfig/test_WifiConfig.cpp
@@ -6,17 +6,15 @@
 #include <unity.h>
 
 #include "test_WifiConfig.h"
+#include "../../lib/WifiConfig/AbstractWifiStorage.h"
 #include <WifiConfigStorage.h>
 #include <cstring>
 
 using namespace fakeit;
 namespace TestWifiConfig {
-    WifiConfigStorage *subjectUnderTest;
-    Logging logging;
-    Mock <AbstractWifiStorage> storage;
-    struct StorageData wifiStorage;
 
-    void setUp() {
+    static struct StorageData createWifiStorageMinimal() {
+        struct StorageData wifiStorage;
         memcpy(wifiStorage.configValid, "NB", 3);
         WifiStorage nullstorage{};
         memset(nullstorage.AccessPointName, '\0', ACCESSPOINT_NAME_LENGTH);
@@ -24,20 +22,60 @@ namespace TestWifiConfig {
         for (int i : {0, 1, 2, 3, 4}) {
             wifiStorage.knownNets[i] = nullstorage;
         }
+        return wifiStorage;
+    }
+
+    WifiConfigStorage *subjectUnderTest;
+    Logging logging;
+    Mock <AbstractWifiStorage> storage;
+
+    void setUp() {
         AbstractWifiStorage &persistence = storage.get();
         subjectUnderTest = new WifiConfigStorage(&logging, &persistence);
     }
 
 
     void test_initStorage() {
+        storage.ClearInvocationHistory();
+        struct StorageData wifiStorage = createWifiStorageMinimal();
         When(Method(storage, begin)).Return();
-        When(Method(storage, get)).AlwaysDo([](StorageData& result) {
+        When(Method(storage, get)).AlwaysDo([wifiStorage](StorageData &result) {
             result = wifiStorage;
         });
-        subjectUnderTest->initStorage();
+        try {
+            subjectUnderTest->initStorage();
+        } catch (UnexpectedMethodCallException e) {
+            TEST_FAIL_MESSAGE(e.what().c_str());
+        }
+        Verify(Method(storage, begin)).Once();
+        Verify(Method(storage, get)).Once();
+        VerifyNoOtherInvocations(storage);
+    }
+
+    void test_initStorageInvalidStorage() {
+        storage.ClearInvocationHistory();
+        struct StorageData wifiStorage = createWifiStorageMinimal();
+        memcpy(wifiStorage.configValid, "NV",3);
+        When(Method(storage, begin)).Return();
+        When(Method(storage, get)).AlwaysDo([wifiStorage](StorageData &result) {
+            result = wifiStorage;
+        });
+        When(Method(storage, put)).Return();
+        When(Method(storage, commit)).Return();
+        try {
+            subjectUnderTest->initStorage();
+        } catch (UnexpectedMethodCallException e) {
+            TEST_FAIL_MESSAGE(e.what().c_str());
+        }
+        Verify(Method(storage, begin)).Once();
+        Verify(Method(storage, get)).Once();
+        Verify(Method(storage, put)).Once();
+        Verify(Method(storage, commit)).Once();
+        VerifyNoOtherInvocations(storage);
     }
 
     void run_tests() {
         RUN_TEST(test_initStorage);
+        RUN_TEST(test_initStorageInvalidStorage);
     }
 }
